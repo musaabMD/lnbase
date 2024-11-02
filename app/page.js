2bcdcfb51c78da525fbf187f66a1c8762d9e2ce8
musaabMD <mousab.r@outlook.com>
2024-11-02 21:11:18 +0300

    v1.1

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import LandingPage from "@/components/Heropage";
 import ComparisonSection from "@/components/Compare";
 import RevenueCalculator from "@/components/reve";
+import { Analytics } from "@vercel/analytics/react"
 export default function Page() {
   return (
     <>
